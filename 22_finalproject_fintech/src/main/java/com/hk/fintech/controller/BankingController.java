3c9688b4a125631be69ee7ef1e316836da2d01f2
HKEDU <HKEDU@DESKTOP-UMP4GPR>
2024-01-04 10:05:17 +0900

    ss

--- 22_finalproject_fintech/src/main/java/com/hk/fintech/controller/BankingController.java
+++ 22_finalproject_fintech/src/main/java/com/hk/fintech/controller/BankingController.java
@@ -271,20 +271,6 @@ public class BankingController {
       return formatNow;
    }
    
-
-   // 계좌 삭제
-   @ResponseBody
-   @DeleteMapping("/deleteAccount")
-   public ResponseEntity<String> deleteAccount(@RequestParam String fintech_use_num) {
-       try {
-           // 여기에서 계좌 삭제 로직을 호출
-           accountService.deleteAccount(fintech_use_num);
-           return new ResponseEntity<>("success", HttpStatus.OK);
-       } catch (Exception e) {
-           e.printStackTrace();
-           return new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
-       }
-   }
    
 //   @GetMapping("/chart")
 //   public String chart() {
